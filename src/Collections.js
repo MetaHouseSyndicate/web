63936f0a913bed8a9c9d05eed94c5f968ea361a9
BazzaDEV <bazarjani.kian@gmail.com>
2022-01-20 11:23:41 -0500

    Add dummy pages

---
+++ src/Collections.js
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const Collections = () => {
+    return (
+        <Box>
+            <h2>Collections</h2>
+        </Box>
+    );
+}
+
+export default Collections;
