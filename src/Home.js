4ed38bd8fc0360f3727bbab7e474bdb65f40b270
BazzaDEV <bazarjani.kian@gmail.com>
2022-01-21 14:17:49 -0500

    Release new collection

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { Discord, Facebook, Instagram, Twitter } from '@icons-pack/react-simple-icons';
+import NewReleasesTwoToneIcon from '@mui/icons-material/NewReleasesTwoTone';
 import { NFTCalendar, OpenSea } from './CustomIcons';
 import { Box, Button, Link, Stack, Typography } from '@mui/material';
 import Countdown from './Countdown';
@@ -8,14 +9,84 @@ import './Home.css';
 const iconSize = 48;
 
 const socials = [
-    { name: 'OpenSea', url: 'https://opensea.io/MetaHouseSyndicate', html: <OpenSea className='icon' width={iconSize} height={iconSize} />},
-    { name: 'Facebook', url: 'https://facebook.com/metahousesyndicate', html: <Facebook className='icon' size={iconSize} />},
-    { name: 'Instagram', url: 'https://instagram.com/metahousesyndicate', html: <Instagram className='icon' size={iconSize} />},
-    { name: 'Twitter', url: 'https://twitter.com/MHSyndicate', html: <Twitter className='icon' size={iconSize} />},
-    { name: 'Discord', url: 'https://discord.gg/JGrxxGB8aK', html: <Discord className='icon' size={iconSize} />}
+    { name: 'OpenSea', url: 'https://opensea.io/MetaHouseSyndicate', html: <OpenSea className='icon' width={iconSize} height={iconSize} /> },
+    { name: 'Facebook', url: 'https://facebook.com/metahousesyndicate', html: <Facebook className='icon' size={iconSize} /> },
+    { name: 'Instagram', url: 'https://instagram.com/metahousesyndicate', html: <Instagram className='icon' size={iconSize} /> },
+    { name: 'Twitter', url: 'https://twitter.com/MHSyndicate', html: <Twitter className='icon' size={iconSize} /> },
+    { name: 'Discord', url: 'https://discord.gg/JGrxxGB8aK', html: <Discord className='icon' size={iconSize} /> }
 ];
 
 const Home = () => {
+
+    const TokensOfHonorReleaseDate = moment('21/01/2022 1:30:00 pm', 'DD/MM/YYYY h:mm:ss a').tz('America/Toronto');
+
+    const HeaderCountdown = () => {
+        return (
+            <Link href='https://opensea.io/collection/tokensofhonor' sx={{ color: 'inherit', textDecoration: 'none' }}>
+                <Countdown
+                    endDate={TokensOfHonorReleaseDate}
+                    fontFamily='Lato'
+                    fontSize={{ xs: '2.9em', sm: '6em' }}
+                    fontWeight='300'
+                    marginBottom={2}
+                />
+
+                <Stack direction='horizontal' justifyContent='center' alignItems='center' sx={{ mb: 4 }}>
+                    <Button
+                        disableRipple
+                        disableElevation
+                        sx={{ p: 0, ":hover": { background: 'transparent' } }}
+                    >
+                        <NFTCalendar color='black' width={40} height={40} />
+                    </Button>
+                    <Typography sx={{
+                        fontFamily: 'Lato',
+                        fontWeight: '300',
+                        fontSize: { xs: '1.2em', sm: '1.7em' },
+                        textAlign: 'center'
+                    }}>
+                        Tokens of Honor Collection drops today at 1:30PM EST.
+                    </Typography>
+                </Stack>
+            </Link>
+        );
+    }
+
+    const HeaderRelease = () => {
+        return (
+            <Link href='https://opensea.io/collection/tokensofhonor' sx={{ color: 'inherit', textDecoration: 'none' }}>
+                <Stack 
+                    direction='horizontal' 
+                    justifyContent='center' 
+                    alignItems='center' 
+                    sx={{ 
+                        fontSize: { xs: '1.2em', sm: '1.7em' }, 
+                        mb: 4,
+                        pt: 0.5,
+                        ':hover': {
+                            textDecoration: 'underline'
+                        }
+                    }}
+                >
+                    <Button
+                        disableRipple
+                        disableElevation
+                        sx={{ p: 0, ":hover": { background: 'transparent' } }}
+                    >
+                        <NewReleasesTwoToneIcon sx={{ m: 0, p: 0, color: 'black', fontSize: { xs: 30, sm: 40 } }} />
+                    </Button>
+                    <Typography sx={{
+                        fontFamily: 'Lato',
+                        fontWeight: '300',
+                        fontSize: 'inherit',
+                        textAlign: 'center', 
+                    }}>
+                        Tokens of Honor Collection <b textDecoration='inherit'>available now</b> on OpenSea.
+                    </Typography>
+                </Stack>
+            </Link>
+        )
+    }
 
     return (
         <div style={{
@@ -32,36 +103,16 @@ const Home = () => {
 
                 <Box
                     paddingX={5}
-                    marginBottom={{ xs: 4, sm: 4 }} 
+                    marginBottom={{ xs: 4, sm: 4 }}
                 >
 
-                    <Link href='https://nftcalendar.io/event/tokens-of-honor/' sx={{ color: 'inherit', textDecoration: 'none' }}>
-                    <Countdown
-                        endDate={moment('21/01/2022', 'DD/MM/YYYY').tz('America/Toronto')}
-                        fontFamily='Lato'
-                        fontSize={{ xs: '2.9em', sm: '6em' }}
-                        fontWeight='300'
-                        marginBottom={2}
-                    />
-
-                    <Stack direction='horizontal' justifyContent='center' alignItems='center' sx={{ mb: 4 }}>
-                        <Button 
-                            disableRipple 
-                            disableElevation 
-                            sx={{ p: 0, ":hover": { background: 'transparent' } }}
-                        >
-                            <NFTCalendar color='black' width={40} height={40} />
-                        </Button>
-                        <Typography sx={{
-                            fontFamily: 'Lato', 
-                            fontWeight: '300', 
-                            fontSize: { xs: '1.2em', sm: '1.7em' }, 
-                            textAlign: 'center' 
-                        }}>
-                            Tokens of Honor Collection drops on January 21 2022.
-                        </Typography>
-                    </Stack>
-                    </Link>
+                    {
+                        TokensOfHonorReleaseDate.diff(moment()) > 0
+                        // moment().diff(moment()) > 0
+                        // false
+                            ? <HeaderCountdown />
+                            : <HeaderRelease />
+                    }
 
                 </Box>
 
@@ -76,7 +127,7 @@ const Home = () => {
                 </Box>
 
                 <Stack marginBottom={4} spacing={{ xs: 2, sm: 4 }} direction='row' justifyContent='center'>
-                    { socials.map((social) => (
+                    {socials.map((social) => (
                         <Link key={social.name + '-link'} href={social.url}>{social.html}</Link>
                     ))}
                 </Stack>
