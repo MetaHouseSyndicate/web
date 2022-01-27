5361c85f2cdcff7c3f27a1705660ecf732e5ea1a
BazzaDEV <bazarjani.kian@gmail.com>
2022-01-26 21:36:55 -0500

    Use single state list instead of multiple states for timer

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,48 +1,39 @@
 import { Box } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 var moment = require('moment-timezone');
 
 const Countdown = ({ endDate, fontFamily, fontSize, fontWeight, marginBottom }) => {
 
-    const [timerDays, setTimerDays] = useState('00');
-    const [timerHours, setTimerHours] = useState('00');
-    const [timerMinutes, setTimerMinutes] = useState('00');
-    const [timerSeconds, setTimerSeconds] = useState('00');
+    const timerZero = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-    let interval = useRef();
-
-    const startTimer = () => {
-
-        interval = setInterval(() => {
-            
-            if (endDate.diff(moment()) < 0) {
-                clearInterval(interval.current);
-
-            } else {
-                const days = endDate.diff(moment(), 'days');
-                const hours = endDate.diff(moment(), 'hours') % 24;
-                const minutes = endDate.diff(moment(), 'minutes') % 60;
-                const seconds = endDate.diff(moment(), 'seconds') % 60;
-
-                setTimerDays( toString(days) );
-                setTimerHours( toString(hours) );
-                setTimerMinutes( toString(minutes) );
-                setTimerSeconds( toString(seconds) );
-
-            }
-    
-            
-        }, 1000);
-        
-    }
+    const [timer, setTimer] = useState(timerZero);
 
     useEffect(() => {
-        startTimer();
-        return () => {
-            clearInterval(interval.current);
+
+        const stopTimer = () => {
+            clearInterval(interval);
         }
-    })
+
+        const interval = setInterval(() => {
+                
+            if (endDate.diff(moment()) < 0) {
+                stopTimer();
+
+            } else {
+                const daysLeft = endDate.diff(moment(), 'days');
+                const hoursLeft = endDate.diff(moment(), 'hours') % 24;
+                const minutesLeft = endDate.diff(moment(), 'minutes') % 60;
+                const secondsLeft = endDate.diff(moment(), 'seconds') % 60;
+
+                setTimer({ days: daysLeft, hours: hoursLeft, minutes: minutesLeft, seconds: secondsLeft });
+
+            }
+
+        }, 1000);
+        
+        return () => stopTimer();
+    }, [endDate]);
 
     return (
         <Box
@@ -55,7 +46,7 @@ const Countdown = ({ endDate, fontFamily, fontSize, fontWeight, marginBottom }) 
             fontSize={fontSize} 
             // backgroundColor='gray'
         >
-            {timerDays} : {timerHours} : {timerMinutes} : {timerSeconds}
+            {stringify(timer.days)} : {stringify(timer.hours)} : {stringify(timer.minutes)} : {stringify(timer.seconds)}
         </Box>
         
     );
@@ -65,7 +56,7 @@ Countdown.defaultProps = {
     endDate: moment().tz('America/Toronto'),
 }
 
-function toString(val) {
+function stringify(val) {
     if (val < 10) return '0' + String(val);
     else return String(val);
 }
